c71d534caa313419b7153918991fd76928ceaf76
Dr.R <djent-@windows>
2018-06-20 19:55:15 -0400

    Part 1-3

--- engine.py
+++ engine.py
@@ -12,15 +12,22 @@ def main():
 
 	tcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
+	con = tcod.console_new(screen_width, screen_height)
+
 	key = tcod.Key()
 	mouse = tcod.Mouse()
 
 	while not tcod.console_is_window_closed():
 		tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
-		tcod.console_set_default_foreground(0, tcod.white)
-		tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
+		
+		tcod.console_set_default_foreground(con, tcod.white)
+		tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
+		tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
 		tcod.console_flush()
 
+		# Erase old @
+		tcod.console_put_char(con, player_x, player_y, ' ', tcod.BKGND_NONE)
+		
 		action = handle_keys(key)
 
 		move = action.get('move')
